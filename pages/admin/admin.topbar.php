acda09a4b91d505123e6df7417bb6f2bb6982419
Drew <andrewsiruma02@gmail.com>
2024-12-02 16:24:35 +0800

    ready the notification bell button

--- pages/admin/admin.topbar.php
+++ pages/admin/admin.topbar.php
@@ -7,8 +7,11 @@
 <!-- Topbar Navbar -->
 <ul class="navbar-nav ml-auto">
 
-
-
+    <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bell"></i>
+        </a>
+    </li>
 
     <div class="topbar-divider d-none d-sm-block"></div>
 
