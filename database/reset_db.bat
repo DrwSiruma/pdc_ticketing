295ced13044c520be074a13e1f24cdf20eae5c1e
Andrew Siruma <163843542+DrwSiruma@users.noreply.github.com>
2025-07-04 11:50:14 +0800

    update

--- database/reset_db.bat
+++ database/reset_db.bat
@@ -6,12 +6,13 @@ REM Set your MySQL credentials here
 set DB_USER=root
 set DB_PASS=
 set DB_NAME=pdc_ticketing_db
+set MYSQL_BIN="D:\xampp\mysql\bin\mysql.exe"
 
 REM Truncate all tables except useraccounts
-mysql -u%DB_USER% -p%DB_PASS% -e "TRUNCATE tbl_auditlog; TRUNCATE tbl_notif; TRUNCATE tbl_ticketreport; TRUNCATE tbl_tickets; TRUNCATE tbl_itemcategory; TRUNCATE tbl_itemlist;" %DB_NAME%
+%MYSQL_BIN% -u%DB_USER% -p%DB_PASS% -e "TRUNCATE tbl_auditlog; TRUNCATE tbl_notif; TRUNCATE tbl_ticketreport; TRUNCATE tbl_tickets; TRUNCATE tbl_itemcategory; TRUNCATE tbl_itemlist;" %DB_NAME%
 
 REM Delete all users except admin (id=1)
-mysql -u%DB_USER% -p%DB_PASS% -e "DELETE FROM tbl_useraccounts WHERE id <> 1;" %DB_NAME%
+%MYSQL_BIN% -u%DB_USER% -p%DB_PASS% -e "DELETE FROM tbl_useraccounts WHERE id <> 1;" %DB_NAME%
 
 echo Database cleanup complete. Only admin user remains.
 pause
